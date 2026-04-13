ccbfd1080d65e7d670379aa771bfcc9a0723eff6
Joyjit Das <joyjitdas.tech@gmail.com>
2026-04-14 00:28:46 +0530

    #Infix to postfix

--- tut90.py
+++ tut90.py
@@ -105,11 +105,45 @@ def prefix2infix(s):
 
     return stack[-1]
 
+def postfix2prefix(s):
+    stack = []
+    for ch in s:
+        if ch.isalnum():
+            stack.append(ch)
+
+        else:
+            op2 = stack.pop()
+            op1 = stack.pop()
+
+            new = f"{ch}{op1}{op2}"
+            stack.append(new)
+
+    return stack[-1]
+def prefix2postfix(s):
+    stack = []
+    for ch in s[::-1]:
+        if ch.isalnum():
+            stack.append(ch)
+
+        else:
+            op1 = stack.pop()
+            op2 = stack.pop()
+
+            new = f"{op1}{op2}{ch}"
+            stack.append(new)
+
+    return stack[-1]
+
+
 s = "a+b*(c^d-e)"
 s1 = "(a+b)*c-d+f"
 s2 = "ab-de+f*/"
 s3 = "*+pq-mn"
+s4 = "ab-de+f*/"
+s5 = "*+pq-mn"
 print(infix2postfix(s))
+print(infix2prefix(s1))
 print(postfix2infix(s2))
 print(prefix2infix(s3))
-print(infix2prefix(s1))
+print(postfix2prefix(s4))
+print(prefix2postfix(s5))
